4f443767ae76268fdcbf08155a90f509d984cd1c
Gustavo Luigi <=>
2024-04-25 22:57:19 -0300

    .

---
+++ lang/pt/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'required' => 'O campo :attribute é obrigatório.',
+    'max' => [
+        'string' => 'O campo :attribute não pode ter mais de :max caracteres.',
+    ],
+    'email' => 'O campo :attribute deve ser um endereço de e-mail válido.',
+];
